472768b7188a57dc023b855fd9f1fca2ec1d6786
Saket Shukla <saket@assystant.com>
2024-04-22 18:46:35 +0530

    Colour updates

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
   event.preventDefault();
-  document.getElementById('result').innerText = "";
+  document.getElementById('result').innerHTML = "";
   const loginname = document.getElementById('loginname').value;
   const password = document.getElementById('password').value;
   if(loginname === "masnsa" && password === "masnsa_pass") {
-    document.getElementById('result').innerText = "Login Successful";
+    document.getElementById('result').innerHTML = '<span class="text-success">Login Successful</span>';
     setTimeout(() => {
       debugger;
-      document.getElementById('result').innerText = "";
+      document.getElementById('result').innerHTML = "";
       document.getElementById('loginForm').classList.add('collapse');
       document.getElementById('mbtiForm').classList.remove('collapse');
     }, 1000);
   } else {
-    document.getElementById('result').innerText = "Failed to login";
+    document.getElementById('result').innerHTML = '<span class="text-danger">Failed to login</span>';
   }
 })
 
 document.getElementById('mbtiForm').addEventListener('submit', async function(event) {
   event.preventDefault(); // Prevent default form submission
-  document.getElementById('result').innerText = "";
+  document.getElementById('result').innerHTML = "";
 
-  const username = document.getElementById('username').value;
+  const username = document.getElementById('username').value.toLowerCase();
   try {
     const response = await fetch('./input.csv');
     const data = await response.text();
     const mbtiData = parseCSV(data);
-    const userMBTI = mbtiData.find(data => data.username === username);
+    const userMBTI = mbtiData.find(data => data.username.toLowerCase() === username);
 
     if (userMBTI) {
-      document.getElementById('result').innerText = `Hello ${username}, your MBTI is: ${userMBTI.mbti.toUpperCase()}`;
+      document.getElementById('result').innerHTML = `Hello ${username}, your MBTI is: <b class="text-primary">${userMBTI.mbti.toUpperCase()}</b>`;
     } else {
-      document.getElementById('result').innerText = `Sorry, no MBTI found for ${username}`;
+      document.getElementById('result').innerHTML = `<span class="text-danger">Sorry, no MBTI found for <b>${username}</b></span>`;
     }
   } catch (error) {
     console.error('Error fetching data:', error);
-    document.getElementById('result').innerText = 'An error occurred. Please try again later.';
+    document.getElementById('result').innerHTML = '<span class="text-danger">An error occurred. Please try again later.</span>';
   }
 });
 
